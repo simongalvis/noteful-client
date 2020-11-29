304447d8fbf95ffc905d0e9905e7c070e7b18b6a
Simon Galvis <simongalvis9@gmail.com>
2020-11-28 23:11:36 -0700

    Refactor for smoother DELETE experience

--- src/Note/Note.js
+++ src/Note/Note.js
@@ -26,13 +26,14 @@ export default class Note extends React.Component {
       .then(res => {
         if (!res.ok)
           return res.json().then(e => Promise.reject(e))
-        return res.json()
+        
       })
       .then(() => {
         this.context.deleteNote(noteId)
         // allow parent to perform extra behaviour
         this.props.onDeleteNote(noteId)
       })
+     
       .catch(error => {
         console.error({ error })
       })
