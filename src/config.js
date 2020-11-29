aaa652c9d7175f856b50ba3e2073d7d79778f45e
Simon Galvis <simongalvis9@gmail.com>
2020-11-29 02:03:12 -0700

    Add remote address for API_ENDPOINT

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-  //API_ENDPOINT: 'http://localhost:9090'
-  API_ENDPOINT: 'http://localhost:8007/api'
+  API_ENDPOINT: 'https://lit-thicket-38921.herokuapp.com/api'
+  //API_ENDPOINT: 'http://localhost:8007/api'
 }
